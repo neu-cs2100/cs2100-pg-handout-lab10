3bde6f9fd6d4cf7a44655074b8357fa4a3bb1006
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-08-14 17:49:47 -0400

    Update q2.py

--- q2.py
+++ q2.py
@@ -1,3 +1,11 @@
+"""
+Please implement the powerset() function below according to the comments.
+You will need to write the documentation.
+
+You will need to either add a recursive helper function or add another 
+parameter to powerset() with a default value (like current: set[int] = set()).
+"""
+
 
 def powerset(inp: set[int]) -> None:
     # Print all possible subsets of inp, each on one line.
@@ -12,7 +20,3 @@ def powerset(inp: set[int]) -> None:
     # [1,2,3]
     # It's okay if the lines are in a different order, as long as all of them are there. Notice that the empty set is included.
     pass
-
-
-
-# You will need to either add a recursive helper function or add another parameter to powerset() with a default value (like current: set[int] = set()).
