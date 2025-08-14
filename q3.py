e497d2017f509e4e5a1fbd3ad60460b4452d79f5
Rasika Bhalerao <rasikabh@users.noreply.github.com>
2025-08-14 17:50:37 -0400

    Update q3.py

--- q3.py
+++ q3.py
@@ -1,5 +1,17 @@
 from typing import Self
 
+"""
+Implement union-find with Users as nodes.
+
+Add two attributes to User:
+1: parent: the User who is this User's parent in the union-find algorithm
+2: rank: an int which represents the node's rank (or height in the tree) for the union-find algorithm
+
+Add these two methods to User:
+1: def find(self) -> Self: finds this User's root / representative node (the "find" step in the union-find algorithm)
+2: def union(self, other: Self) -> None: combines the sets containing this node and other into a single set (the "union" step of the union-find algorithm)
+"""
+
 class User:
     # Keeps track of one person, or a node in a social media graph
     def __init__(self, name: str):
@@ -21,12 +33,3 @@ class User:
     def add_friend(self, friend: Self) -> None:
         self.friends.add(friend)
         friend.friends.add(self)
-
-
-# Implement union-find with Users as nodes.
-# Add two attributes to User:
-# 1: parent: the User who is this User's parent in the union-find algorithm
-# 2: rank: an int which represents the node's rank (or height in the tree) for the union-find algorithm
-# Add these two methods to User:
-# def find(self) -> Self: finds this User's root / representative node (the "find" step in the union-find algorithm)
-# def union(self, other: Self) -> None: combines the sets containing this node and other into a single set (the "union" step of the union-find algorithm)
